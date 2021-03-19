b401e6737493b11c9e353453ec368359383a5439
Justin Wozniak <woz@anl.gov>
2021-03-19 13:59:25 -0500

    Add TODOs and test 3 (which fails)

--- cr-nt3-1.sh
+++ cr-nt3-1.sh
@@ -42,10 +42,21 @@ cd        $RUNDIR
 
   python3 $NT3 --epochs 5  |& tee    run-1.out
   check-output.sh "Epoch 5/5"        run-1.out
+  # TODO: Check that we did a successful checksum
 
   python3 $NT3 --epochs 10 |& tee    run-2.out
   check-output.sh "initial_epoch: 5" run-2.out
   check-output.sh "Epoch 10/10"      run-2.out
+  # TODO: Check that we did a successful checksum for read+write
+
+  python3 $NT3 --epochs 3                \
+               --ckpt_checksum=False     \
+               --ckpt_restart_mode="off" \
+                           |& tee    run-3.out
+  # TODO: Check that we did not restart
+  # TODO: Check that we did not try to checksum
+  check-output.sh "Epoch 3/3"        run-3.out
+
 )
 echo
 echo "SUCCESS."
