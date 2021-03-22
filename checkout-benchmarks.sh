baac7efc47de0fa65d722798179f6c5ac6dba40c
Justin Wozniak <woz@anl.gov>
2021-03-22 14:28:34 -0500

    Adding checkout-benchmarks.sh

---
+++ checkout-benchmarks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+set -x
+
+/bin/pwd
+
+if [[ -d Benchmarks ]]
+then
+  cd Benchmarks
+  git pull
+else
+  git clone https://github.com/ECP-CANDLE/Benchmarks.git
+  cd Benchmarks
+  git checkout issue-86
+fi
