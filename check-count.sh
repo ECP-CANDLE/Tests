39625f93b1064906b7e0eac610937085661c78d0
Justin Wozniak <woz@anl.gov>
2021-03-22 14:17:07 -0500

    Test keep limit

---
+++ check-count.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+# CHECK COUNT SH
+
+# Check for a COUNT
+
+ACTUAL=$1  # Number observed
+COUNT=$2   # Number expected
+FILE=$3    # Test run output file
+MSG="$4"   # Extra human-readable error message
+if (( ACTUAL != COUNT ))
+then
+  echo "cr-nt3-1: check(): ACTUAL='$ACTUAL' != COUNT='$COUNT' in $FILE"
+  echo "                   $MSG"
+  exit 1
+fi
--- cr-nt3-1.sh
+++ cr-nt3-1.sh
@@ -40,21 +40,38 @@ cd        $RUNDIR
   set -x
   /bin/pwd
 
+  # 1: Fresh run w/ checksums
   python3 $NT3 --epochs 5 --ckpt_checksum True |& tee    run-1.out
   check-output.sh "Epoch 5/5"        run-1.out
   check-output.sh "checksummed:"     run-1.out
 
+  # 2: Restart run w/ checksums
   python3 $NT3 --epochs 10 --ckpt_checksum True |& tee    run-2.out
   check-output.sh "initial_epoch: 5" run-2.out
   check-output.sh "Epoch 10/10"      run-2.out
   check-output.sh "checksummed:"     run-2.out
 
+  # 3: Fresh run w/o checksums
   python3 $NT3 --epochs 3                \
                --ckpt_restart_mode "off" \
                            |& tee    run-3.out
   check-output.sh -n "restarting:"   run-3.out
   check-output.sh -n "checksummed:"  run-3.out
   check-output.sh    "Epoch 3/3"     run-3.out
+
+  # 4: Fresh run, limit checkpoints kept
+  rm -rfv runs/cr-nt3-1/save/ckpts/epochs
+  python3 $NT3 --epochs 10               \
+               --ckpt_restart_mode "off" \
+               --ckpt_save_best False    \
+               --ckpt_keep_limit 4       \
+                           |& tee    run-4.out
+  check-output.sh -n "restarting:"   run-4.out
+  check-output.sh -n "checksummed:"  run-4.out
+  check-output.sh    "Epoch 10/10"   run-4.out
+  check-output.sh    "Epoch 10/10"   run-4.out
+  CKPTS=( $( find . -name '*.h5' ) )
+  check-count.sh ${#CKPTS[@]} 4 run-4.out "ckpt count is wrong"
 )
 
 echo
