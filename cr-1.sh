6c28745cc8eb9716ab4c4b79dc746233bc02bb48
Justin Wozniak <woz@anl.gov>
2022-08-27 12:41:20 -0500

    Fix alignment

--- cr-1.sh
+++ cr-1.sh
@@ -24,7 +24,7 @@ source $THIS/setup-test.sh
                --ckpt_checksum True   \
                --ckpt_save_interval 1 \
                            |& tee    run-1.out
-  check-epoch.sh  3        run-1.out
+  check-epoch.sh  3                  run-1.out
   check-output.sh "checksummed:"     run-1.out
 
   # 2: Restart run w/ checksums
@@ -34,7 +34,7 @@ source $THIS/setup-test.sh
                --ckpt_restart_mode "required" \
                                       |& tee run-2.out
   check-output.sh "restarting from epoch: 3" run-2.out
-  check-epoch.sh  5                run-2.out
+  check-epoch.sh  5                          run-2.out
   check-output.sh "checksummed:"             run-2.out
 
   # 3: Fresh run w/o restart/checksums
@@ -44,7 +44,7 @@ source $THIS/setup-test.sh
                            |& tee    run-3.out
   check-output.sh -n "restarting"    run-3.out
   check-output.sh -n "checksummed:"  run-3.out
-  check-epoch.sh     3          run-3.out
+  check-epoch.sh     3               run-3.out
 
   # 4: Fresh run, limit checkpoints kept
   rm -rfv runs/cr-$TOKEN-1/save/ckpts/epochs
@@ -56,7 +56,7 @@ source $THIS/setup-test.sh
                            |& tee    run-4.out
   check-output.sh -n "restarting:"   run-4.out
   check-output.sh -n "checksummed:"  run-4.out
-  check-epoch.sh     10   run-4.out
+  check-epoch.sh     10              run-4.out
   CKPTS=( $( find . -name '*.h5' ) )
   check-count.sh ${#CKPTS[@]} 1 run-4.out "ckpt count is wrong"
 )
