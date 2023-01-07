e194e0dbcf879165a54f5392e4c0a91865d8bcfe
Rajeev Jain <rajeeja@gmail.com>
2023-01-07 14:01:36 -0600

    o Add mnist test for gce

---
+++ workflow/mnist-gce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+echo "MNIST GCE: START " $( date "+%Y-%m-%d %H:%M" )
+
+echo CANDLE_REPO=$CANDLE_REPO
+
+set -x
+
+hostname
+
+cd $CANDLE_REPO/examples/mnist/
+
+pwd
+export CANDLE_DATA_DIR=/nfs/gce/globalscratch/jain/Data
+ls $CANDLE_DATA_DIR
+
+PATH=/nfs/gce/globalscratch/jain/conda_installs/bin:$PATH
+which python
+
+python mnist_candle.py
+
+echo MNIST GCE: STOP" $( date "+%Y-%m-%d %H:%M" )
