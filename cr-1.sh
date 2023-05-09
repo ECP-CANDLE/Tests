c9ffcf99691a1671e202e65367d4937dbb524d04
Justin Wozniak <woz@anl.gov>
2023-05-09 15:50:25 -0500

    Restore tests 1-3, set ckpt_save_best=False

--- cr-1.sh
+++ cr-1.sh
@@ -30,32 +30,32 @@ source $THIS/setup-test.sh
 
   set -o pipefail
 
-  # # 1: Fresh run w/ checksums
-  # python3 $BMK --epochs 3             \
-  #              --ckpt_checksum True   \
-  #              --ckpt_save_interval 1 \
-  #                          |& tee    run-1.out
-  # check-epoch.sh  3                  run-1.out
-  # check-output.sh "checksummed:"     run-1.out
+  # 1: Fresh run w/ checksums
+  python3 $BMK --epochs 3             \
+               --ckpt_checksum True   \
+               --ckpt_save_interval 1 \
+                           |& tee    run-1.out
+  check-epoch.sh  3                  run-1.out
+  check-output.sh "checksummed:"     run-1.out
 
-  # # 2: Restart run w/ checksums
-  # python3 $BMK --epochs 5 \
-  #              --ckpt_checksum True \
-  #              --ckpt_save_interval 1 \
-  #              --ckpt_restart_mode "required" \
-  #                                     |& tee run-2.out
-  # check-output.sh "restarting from epoch: 3" run-2.out
-  # check-epoch.sh  5                          run-2.out
-  # check-output.sh "checksummed:"             run-2.out
+  # 2: Restart run w/ checksums
+  python3 $BMK --epochs 5 \
+               --ckpt_checksum True \
+               --ckpt_save_interval 1 \
+               --ckpt_restart_mode "required" \
+                                      |& tee run-2.out
+  check-output.sh "restarting from epoch: 3" run-2.out
+  check-epoch.sh  5                          run-2.out
+  check-output.sh "checksummed:"             run-2.out
 
-  # # 3: Fresh run w/o restart/checksums
-  # python3 $BMK --epochs 3                \
-  #              --ckpt_restart_mode "off" \
-  #              --ckpt_checksum False     \
-  #                          |& tee    run-3.out
-  # check-output.sh -n "restarting"    run-3.out
-  # check-output.sh -n "checksummed:"  run-3.out
-  # check-epoch.sh     3               run-3.out
+  # 3: Fresh run w/o restart/checksums
+  python3 $BMK --epochs 3                \
+               --ckpt_restart_mode "off" \
+               --ckpt_checksum False     \
+                           |& tee    run-3.out
+  check-output.sh -n "restarting"    run-3.out
+  check-output.sh -n "checksummed:"  run-3.out
+  check-epoch.sh     3               run-3.out
 
   # 4: Fresh run, limit checkpoints kept
   rm -rfv runs/cr-$TOKEN-1/save/ckpts/epochs
@@ -63,6 +63,7 @@ source $THIS/setup-test.sh
                --ckpt_restart_mode "off" \
                --ckpt_keep_limit 1       \
                --ckpt_save_interval 1    \
+               --ckpt_save_best False    \
                --ckpt_checksum False     \
                            |& tee    run-4.out
   check-output.sh -n "restarting:"   run-4.out
