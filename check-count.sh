67141ca3370ab6d89d2d9cba48e59e2da6da95db
Justin Wozniak <woz@anl.gov>
2023-05-10 15:21:34 -0500

    Readability

--- check-count.sh
+++ check-count.sh
@@ -9,9 +9,13 @@ ACTUAL=$1  # Number observed
 COUNT=$2   # Number expected
 FILE=$3    # Test run output file
 MSG="$4"   # Extra human-readable error message
+
 if (( ACTUAL != COUNT ))
 then
   echo "check-count: ACTUAL='$ACTUAL' != COUNT='$COUNT' in $FILE"
   echo "             $MSG"
   exit 1
 fi
+
+# Else success!
+exit 0
