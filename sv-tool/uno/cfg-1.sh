4002464118f286ce49653dfa32eea370890ffb3c
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-01-29 00:44:47 -0800

    update reduce_lr_factor and customize cfg files

--- sv-tool/uno/cfg-1.sh
+++ sv-tool/uno/cfg-1.sh
@@ -2,6 +2,7 @@
 source_cfg -v cfg-my-settings.sh
 
 export CANDLE_MODEL_TYPE="SINGULARITY"
-# export MODEL_NAME=/software/improve/images/<model>.sif  #Lambda
+# export MODEL_NAME=/homes/weaverr/Model_Curation/singularity/uno_improve.sif
+export MODEL_NAME=/software/improve/images/uno_improve.sif  #Lambda
 # export MODEL_NAME=/lus/grand/projects/CSC249ADOA01/images/<model>.sif  #Polaris
 export PARAM_SET_FILE=hyperparams.json
--- sv-tool/uno/cfg-my-settings.sh
+++ sv-tool/uno/cfg-my-settings.sh
@@ -6,20 +6,21 @@ export PROCS=4
 export PPN=4
 export WALLTIME=01:00:00
 export NUM_ITERATIONS=1
-export POPULATION_SIZE=2
+export POPULATION_SIZE=8
 
 # GA Settings
 export GA_STRATEGY='mu_plus_lambda'
 export OFFSPRING_PROPORTION=0.5
 export MUT_PROB=0.8
 export CX_PROB=0.2
-export MUT_INDPB=0.5
-export CX_INDPB=0.5
+export MUT_INDPB=0.8
+export CX_INDPB=0.2
 export TOURNSIZE=4
 
 # Lambda Settings
 # export CANDLE_CUDA_OFFSET=1
-# export CANDLE_DATA_DIR=/tmp/<user>/data_dir
+export IMPROVE_DATA_DIR=/tmp/weaverr/Data
+export CANDLE_DATA_DIR=/tmp/weaverr/Data
 
 # Polaris Settings
 # export QUEUE="debug"
