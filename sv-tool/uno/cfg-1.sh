b67c6ae78f63af90301cd721a495dd9def0dc0fb
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-01-31 01:50:39 -0800

    update files for HPO

--- sv-tool/uno/cfg-1.sh
+++ sv-tool/uno/cfg-1.sh
@@ -2,7 +2,6 @@
 source_cfg -v cfg-my-settings.sh
 
 export CANDLE_MODEL_TYPE="SINGULARITY"
-# export MODEL_NAME=/homes/weaverr/Model_Curation/singularity/uno_improve.sif
 export MODEL_NAME=/software/improve/images/uno_improve.sif  #Lambda
 # export MODEL_NAME=/lus/grand/projects/CSC249ADOA01/images/<model>.sif  #Polaris
-export PARAM_SET_FILE=hyperparams.json
+export PARAM_SET_FILE=hyperparams2.json
--- sv-tool/uno/cfg-my-settings.sh
+++ sv-tool/uno/cfg-my-settings.sh
@@ -2,17 +2,17 @@
 echo SETTINGS
 
 # General Settings
-export PROCS=4
-export PPN=4
+export PROCS=3
+export PPN=3
 export WALLTIME=01:00:00
 export NUM_ITERATIONS=1
-export POPULATION_SIZE=8
+export POPULATION_SIZE=2
 
 # GA Settings
 export GA_STRATEGY='mu_plus_lambda'
 export OFFSPRING_PROPORTION=0.5
-export MUT_PROB=0.8
-export CX_PROB=0.2
+export MUT_PROB=0.3
+export CX_PROB=0.3
 export MUT_INDPB=0.8
 export CX_INDPB=0.2
 export TOURNSIZE=4
