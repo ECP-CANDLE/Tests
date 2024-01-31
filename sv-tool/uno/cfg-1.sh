9762410c68cbff260f0ffe0d7186055378a2badf
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-01-31 01:51:34 -0800

    hyperparams file

--- sv-tool/uno/cfg-1.sh
+++ sv-tool/uno/cfg-1.sh
@@ -4,4 +4,4 @@ source_cfg -v cfg-my-settings.sh
 export CANDLE_MODEL_TYPE="SINGULARITY"
 export MODEL_NAME=/software/improve/images/uno_improve.sif  #Lambda
 # export MODEL_NAME=/lus/grand/projects/CSC249ADOA01/images/<model>.sif  #Polaris
-export PARAM_SET_FILE=hyperparams2.json
+export PARAM_SET_FILE=hyperparams.json
