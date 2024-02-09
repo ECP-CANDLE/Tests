c0d6810939dec2024960970085c1d5474104565a
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-02-09 03:18:08 -0800

    better default settings

--- sv-tool/uno/cfg-my-settings.sh
+++ sv-tool/uno/cfg-my-settings.sh
@@ -2,20 +2,20 @@
 echo SETTINGS
 
 # General Settings
-export PROCS=3
-export PPN=3
-export WALLTIME=01:00:00
-export NUM_ITERATIONS=1
-export POPULATION_SIZE=2
+export PROCS=4
+export PPN=4
+export WALLTIME=12:00:00
+export NUM_ITERATIONS=4
+export POPULATION_SIZE=4
 
 # GA Settings
 export GA_STRATEGY='mu_plus_lambda'
 export OFFSPRING_PROPORTION=0.5
-export MUT_PROB=0.3
-export CX_PROB=0.3
-export MUT_INDPB=0.8
-export CX_INDPB=0.2
-export TOURNSIZE=4
+export MUT_PROB=0.5
+export CX_PROB=0.5
+export MUT_INDPB=0.6
+export CX_INDPB=0.15
+export TOURNSIZE=3
 
 # Lambda Settings
 # export CANDLE_CUDA_OFFSET=1
