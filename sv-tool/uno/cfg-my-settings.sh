58d0d45a910dbc720b32733907af144c2c5726a7
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-02-12 02:58:00 -0800

    comment out IMPROVE_DATA_DIR for people to be able to test UNO HPO

--- sv-tool/uno/cfg-my-settings.sh
+++ sv-tool/uno/cfg-my-settings.sh
@@ -2,25 +2,25 @@
 echo SETTINGS
 
 # General Settings
-export PROCS=4
-export PPN=4
-export WALLTIME=12:00:00
-export NUM_ITERATIONS=4
-export POPULATION_SIZE=4
+export PROCS=20
+export PPN=10
+export WALLTIME=60:00:00
+export NUM_ITERATIONS=9
+export POPULATION_SIZE=60
 
 # GA Settings
 export GA_STRATEGY='mu_plus_lambda'
 export OFFSPRING_PROPORTION=0.5
-export MUT_PROB=0.5
-export CX_PROB=0.5
+export MUT_PROB=0.6
+export CX_PROB=0.4
 export MUT_INDPB=0.6
-export CX_INDPB=0.15
-export TOURNSIZE=3
+export CX_INDPB=0.2
+export TOURNSIZE=4
 
 # Lambda Settings
 # export CANDLE_CUDA_OFFSET=1
-export IMPROVE_DATA_DIR=/tmp/weaverr/Data
-export CANDLE_DATA_DIR=/tmp/weaverr/Data
+# export IMPROVE_DATA_DIR=/tmp/weaverr/Data
+# export CANDLE_DATA_DIR=/tmp/weaverr/Data
 
 # Polaris Settings
 # export QUEUE="debug"
