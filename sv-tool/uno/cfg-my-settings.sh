22221c8e5809727d9fc8015fb3a71f25caa5db02
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-02-22 09:45:33 -0800

    m

--- sv-tool/uno/cfg-my-settings.sh
+++ sv-tool/uno/cfg-my-settings.sh
@@ -2,25 +2,25 @@
 echo SETTINGS
 
 # General Settings
-export PROCS=20
-export PPN=10
-export WALLTIME=60:00:00
-export NUM_ITERATIONS=9
-export POPULATION_SIZE=60
+export PROCS=22
+export PPN=11
+export WALLTIME=48:00:00
+export NUM_ITERATIONS=5
+export POPULATION_SIZE=40
 
 # GA Settings
 export GA_STRATEGY='mu_plus_lambda'
 export OFFSPRING_PROPORTION=0.5
-export MUT_PROB=0.6
-export CX_PROB=0.4
-export MUT_INDPB=0.6
-export CX_INDPB=0.2
+export MUT_PROB=0.8
+export CX_PROB=0.2
+export MUT_INDPB=0.8
+export CX_INDPB=0.5
 export TOURNSIZE=4
 
 # Lambda Settings
 # export CANDLE_CUDA_OFFSET=1
-# export IMPROVE_DATA_DIR=/tmp/weaverr/Data
-# export CANDLE_DATA_DIR=/tmp/weaverr/Data
+export IMPROVE_DATA_DIR=/tmp/weaverr/Data
+export CANDLE_DATA_DIR=/tmp/weaverr/Data
 
 # Polaris Settings
 # export QUEUE="debug"
