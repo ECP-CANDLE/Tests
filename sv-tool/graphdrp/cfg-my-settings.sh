3cc1b7984cc954fdfdeecd80034a1b6def3bac93
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-02-22 17:01:24 -0800

    add graphdrp

---
+++ sv-tool/graphdrp/cfg-my-settings.sh
@@ -0,0 +1,28 @@
+
+echo SETTINGS
+
+# General Settings
+export PROCS=12
+export PPN=6
+export WALLTIME=72:00:00
+export NUM_ITERATIONS=5
+export POPULATION_SIZE=40
+
+# GA Settings
+export GA_STRATEGY='mu_plus_lambda'
+export OFFSPRING_PROPORTION=0.5
+export MUT_PROB=0.8
+export CX_PROB=0.2
+export MUT_INDPB=0.8
+export CX_INDPB=0.5
+export TOURNSIZE=4
+
+# Lambda Settings
+# export CANDLE_CUDA_OFFSET=1
+export IMPROVE_DATA_DIR=/tmp/weaverr/Data
+export CANDLE_DATA_DIR=/tmp/weaverr/Data
+
+# Polaris Settings
+# export QUEUE="debug"
+# export CANDLE_DATA_DIR=/home/<user>/data_dir
+
